c054458aebd14000d0d1f17949900d702a191b7c
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2024-11-29 09:01:19 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function NumRandom(List) {
+    let element = "";
+
+    element = List[Math.floor(Math.random() * List.length)];
+    return element;
+  }
+
+  let generador = document.getElementById("excusa");
+  generador.innerHTML =
+    NumRandom(who) +
+    " " +
+    NumRandom(action) +
+    " " +
+    NumRandom(what) +
+    " " +
+    NumRandom(when);
 };
